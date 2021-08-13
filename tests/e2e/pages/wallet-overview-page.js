a935d02a9060683fc84502cd53fd09bf0e0dd036
Jakub Mucha <jakub.mucha@icloud.com>
2021-08-13 17:13:59 +0200

    e2e: add wallet address input po
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- tests/e2e/pages/wallet-overview-page.js
+++ tests/e2e/pages/wallet-overview-page.js
@@ -1,6 +1,10 @@
 import Page from './page';
 
 export default class WalletOverviewPage extends Page {
+  getWalletAddressInput() {
+    return cy.findByTestId('wallet-address-input').find('input');
+  }
+
   visit() {
     cy.visit('/');
   }
